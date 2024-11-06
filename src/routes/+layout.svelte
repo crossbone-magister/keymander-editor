e3256a72fcfcf265fb6eba7b06e1932c8a7a1801
Ivo Forlin <ivo.forlin@gmail.com>
2024-11-06 18:33:36 +0100

    feat: footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import ErrorDialog from '$lib/components/ErrorDialog.svelte';
 	import { dataLoaded, decks, errorMessage, keymanderDeck } from '$lib/stores';
 	import { setContext } from 'svelte';
+	import { version } from '$app/environment';
 
 	setContext('decks', decks);
 	setContext('keymanderDeck', keymanderDeck);
@@ -14,5 +15,17 @@
 </svelte:head>
 
 <slot></slot>
+<footer class="footer">
+	Created by <a href="https://github.com/crossbone-magister">Crossbone Magister</a> -
+	View on <a href="https://github.com/crossbone-magister/keymander-editor">GitHub</a> -
+	Version {version}
+</footer>
 
 <ErrorDialog />
+
+<style>
+	footer {
+		font-size: smaller;
+		text-align: right;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
 const dev = process.argv.includes('dev');
+const path = fileURLToPath(new URL('package.json', import.meta.url));
+const pkg = JSON.parse(readFileSync(path, 'utf8'));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +16,8 @@ const config = {
 		appDir: 'app',
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH
-		}
+		},
+		version: { name: pkg.version }
 	}
 };
 
